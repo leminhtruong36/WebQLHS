99cf544a246cb3215c4b6a1abc5fbc0106897a04
emtuncoder <vohoanghthien1709@gmail.com>
2024-06-27 07:29:32 +0700

    27/6 7h29

--- Controllers/HocSinhController.cs
+++ Controllers/HocSinhController.cs
@@ -1,14 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebQLHS.Models;
 using WebQLHS.Models.Authentication;
 
 namespace WebQLHS.Controllers
 {
-    [Authentication]
+
+    //[Route("hocsinh")]   
     public class HocSinhController : Controller
     {
+        private readonly QLHS_1Context _context;
+        public HocSinhController(QLHS_1Context context)
+        {
+            _context = context;
+        }
         public IActionResult DanhSachHocSinh()
         {
             return View();
         }
+        [Route("thongtincanhan")]
+        [HttpGet]
+        public IActionResult ThongTinCaNhan()
+        {
+            string maHS = HttpContext.Session.GetString("MaHs");
+            
+            if (!string.IsNullOrEmpty(maHS))
+            {
+                var hocSinh = _context.HocSinhs
+                .FirstOrDefault(hs => hs.MaHs == maHS);
+
+                if (hocSinh == null)
+                {
+                    return NotFound();
+                }
+                return View(hocSinh);
+            }
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
     }
 }
